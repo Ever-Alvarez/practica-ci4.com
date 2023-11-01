47995f757dfa0fe540336383564bb98b56b6c044
Ever-Alvarez <ever.garzaalvarez25@gmail.com>
2023-11-01 16:16:31 -0600

    Se activo el protected  y se modifico el hrfer de index

--- app/Views/index.php
+++ app/Views/index.php
@@ -8,7 +8,7 @@
 <body>
     <nav>
         <ul>
-            <li><a href="/productos" target="_blank">Productos</a></li>
+            <li><a href="/productos/" target="_blank">Productos</a></li>
             <li><a href="/productos/detallesProductos" target="_blank">Detalles Productos</a></li>
         </ul>
     </nav>
