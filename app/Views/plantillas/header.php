361be8f46d7f1b3a0bbffe51e9bf406ea158ae70
Ever-Alvarez <ever.garzaalvarez25@gmail.com>
2023-11-06 11:28:57 -0600

    Se reinstalaron los favicon2

--- app/Views/plantillas/header.php
+++ app/Views/plantillas/header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- favicon START -->
-    <link rel="apple-touch-icon" sizes="57x57" href="/img/favicon1/apple-icon-57x57.png">
+    <!-- <link rel="apple-touch-icon" sizes="57x57" href="/img/favicon1/apple-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="/img/favicon1/apple-icon-60x60.png">
     <link rel="apple-touch-icon" sizes="72x72" href="/img/favicon1/apple-icon-72x72.png">
     <link rel="apple-touch-icon" sizes="76x76" href="/img/favicon1/apple-icon-76x76.png">
@@ -20,7 +20,7 @@
     <link rel="manifest" href="/img/favicon1/manifest.json">
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="/img/favicon1/ms-icon-144x144.png">
-    <meta name="theme-color" content="#ffffff">
+    <meta name="theme-color" content="#ffffff"> -->
   <!-- favicon END -->
     <title><?php echo $titulo; ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
